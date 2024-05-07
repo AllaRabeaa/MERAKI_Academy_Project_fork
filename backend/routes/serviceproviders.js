08bb171ae5be8f660c7e6f0e3deb2bea78a9dd6d
AllaRabeaa <allaa.emad.rabei@gmail.com>
2024-05-07 21:30:07 +0300

    edit create provider

--- backend/routes/serviceproviders.js
+++ backend/routes/serviceproviders.js
@@ -8,14 +8,18 @@ const { createServiceProvider,
     deleteServiceProvider }
     = require("../controllers/service_provider")
 
+    const authentication = require("../middleware/authentication");
+    const authorization = require("../middleware/authorization");
 
-serviceProvidersRouter.post('/', createServiceProvider)
 
-serviceProvidersRouter.put('/:id', updateServiceProviderById)
+
+serviceProvidersRouter.post('/',authentication,authorization("create_service_provider"), createServiceProvider)
+
+serviceProvidersRouter.put('/:id',authentication,authorization("create_service_provider"), updateServiceProviderById)
 
 serviceProvidersRouter.get('/', readAllServiceProvider)
 
-serviceProvidersRouter.delete('/:id', deleteServiceProvider) 
+serviceProvidersRouter.delete('/:id',authentication,authorization("create_service_provider"), deleteServiceProvider) 
 
 
 module.exports = serviceProvidersRouter;
